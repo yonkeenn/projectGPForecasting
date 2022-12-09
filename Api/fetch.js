8644e1076c2271b54a58e164e5d8d8dfc2fbf48b
yonkeenn <jhon.vargasr@gmail.com>
2022-12-08 23:28:38 -0500

    Updating Api

--- Api/fetch.js
+++ Api/fetch.js
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch';
+import fs from "fs";
+
 // api url
 const api_url = "https://api.zippopotam.us/us/33162";
   
+const database = [];
 
 // Defining async function
 async function getapi(url) {
@@ -10,8 +13,29 @@ async function getapi(url) {
     const response = await fetch(url);
     
     // Storing data in form of JSON
-    var data = await response.json();
+    const data = await response.json();
+
+    const jdata = JSON.stringify(data);
+    // database.push(data);
     console.log(data);
+
+    fs.writeFile("test.json", jdata, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    });
 }
+
+
+
 // Calling that async function
 getapi(api_url);
+
+
+
+fs.writeFile('helloworld.txt', 'Hello World!', function (err) {
+  if (err) return console.log(err);
+  console.log('Hello World > helloworld.txt');
+});
+
+console.log(database);
